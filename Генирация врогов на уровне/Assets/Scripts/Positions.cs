50522220013094211f41298ae88de9a51af57057
Владислав161 <64612715+apex161rus@users.noreply.github.com>
2022-06-18 05:53:46 +0300

    test 3

---
+++ Генирация врогов на уровне/Assets/Scripts/Positions.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Positions : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _spawnPositions;
+
+    public GameObject[] Spawn => _spawnPositions;
+}
---
+++ Генирация врогов на уровне/Assets/Scripts/Spawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Positions))]
+
+public class Spawn : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemy;
+
+    private Positions _positions;
+
+    private void Start()
+    {
+        _positions = GetComponent<Positions>();
+        StartCoroutine(SpawnEnemies());
+    }
+
+    private IEnumerator SpawnEnemies()
+    {
+        var waitForSeconds = new WaitForSeconds(2f);
+
+        foreach (var enemy in _positions.Spawn)
+        {
+            yield return waitForSeconds;
+            Instantiate(_enemy, enemy.transform.position, Quaternion.identity);
+        }
+    }
+}
